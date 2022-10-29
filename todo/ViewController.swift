ca968dae110bf268ea8c65961bf98b91671d5e62
itcrown2010 <1409150339@qq.com>
2022-10-29 22:20:43 +0800

    seconde commit

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TodoListViewController: UITableViewController {
+    var itemArray = ["购买水杯","吃药","修改密码"];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //test
     }
 
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "添加一个新的Todo项目", message:  "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "添加项目", style: .default) { (action) in
+           
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "创建一个新项目..."
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+       
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
